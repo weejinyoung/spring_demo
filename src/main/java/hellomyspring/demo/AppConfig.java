1e532f2bccf9baeceaa7a622bb19edf91601bbe3
weejinyoung <a8118199@gmail.com>
2022-01-25 12:24:14 +0900

    Spring Bean 적용

--- src/main/java/hellomyspring/demo/AppConfig.java
+++ src/main/java/hellomyspring/demo/AppConfig.java
@@ -1,21 +1,37 @@
 package hellomyspring.demo;
 
+import hellomyspring.demo.discount.DiscountPolicy;
 import hellomyspring.demo.discount.FixDiscountPolicy;
+import hellomyspring.demo.discount.RateDiscountPolicy;
+import hellomyspring.demo.member.MemberRepository;
 import hellomyspring.demo.member.MemberService;
 import hellomyspring.demo.member.MemberServiceImpl;
 import hellomyspring.demo.member.MemoryMemberRepository;
 import hellomyspring.demo.order.OrderService;
 import hellomyspring.demo.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/hellomyspring/demo/order/OrderApp.java
+++ src/main/java/hellomyspring/demo/order/OrderApp.java
@@ -4,13 +4,19 @@ import hellomyspring.demo.AppConfig;
 import hellomyspring.demo.member.Grade;
 import hellomyspring.demo.member.Member;
 import hellomyspring.demo.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        // AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+        //OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
